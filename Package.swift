aa073d3ef7fa8dad1cab56cc5f845aeef87e8c8d
yangliu-1995 <95995778+yangliu-1995@users.noreply.github.com>
2023-08-06 02:51:19 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(
             name: "FFmpeg-iOS-Support",
             targets: [
-                "Depend", "Hook", "FFmpegSupport",
+                "Depend", "Hook", "FFmpegSupport", "libavdevice", "libfftools"
             ]),
     ],
     targets: [
@@ -29,5 +29,7 @@ let package = Package(
         .target(name: "FFmpegSupport", dependencies: [
             "Hook",
         ]),
+        .binaryTarget(name: "libavdevice", url: "https://github.com/kewlbear/FFmpeg-iOS/releases/download/v0.0.6-b20230416-173821/avdevice.zip", checksum: "5e8bebaae6ef03dfeab3166fa34852728a4c1eac40d9dafcd6b4cb1f71055174"),
+        .binaryTarget(name: "libfftools", url: "https://github.com/kewlbear/FFmpeg-iOS/releases/download/v0.0.6-b20230416-173821/fftools.zip", checksum: "da3e909deb1ee98ef2425a2a4640b663fbb84ecc4204d663b1959c8de587acc7"),
     ]
 )
